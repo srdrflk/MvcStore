ca1ccc1723531dc69ff57440431bf13077c7b4ec
Serdar <srdrflk@hotmail.com>
2020-05-06 16:33:58 +0300

    MvcStoreApp

---
+++ MvcStore/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using MvcStore.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcStore.Controllers
+{
+    public class LoginController : Controller
+    {
+        DbMvcMagazaEntities db = new DbMvcMagazaEntities();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tblAdmin p)
+        {
+            var li = db.tblAdmin.FirstOrDefault(x => x.Kullanici == p.Kullanici && x.Sifre == p.Sifre);
+            if (li != null)
+            {
+                FormsAuthentication.SetAuthCookie(li.Kullanici, false);
+                return RedirectToAction("Index", "Musteri");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
